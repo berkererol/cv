a623c008bd71bc74bfaab458ea100bec75680661
Berker Erol <berkererol@gmail.com>
2021-02-02 23:19:44 -0500

    added certificate, styled pdf.

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -4,10 +4,10 @@ module.exports = {
   facts: {
     "Address:": "Montréal, QC - H2W 1C9",
     "Github:": `<a href="https://github.com/berkererol">
-                  <span class="print">https://github.com/berkererol</span>
+                  <span class="print">github.com/berkererol</span>
                   <span class="screen">repositories</span>
                 </a>`,
-    "Linkedin:": '<a href="https://www.linkedin.com/in/berkererol/">profile</a>',
+    "Linkedin:": '<a class="screen" href="https://www.linkedin.com/in/berkererol/">profile</a><a class="print" href="https://www.linkedin.com/in/berkererol/">linkedin.com/in/berkererol</a>',
     "Phone:": "+1-514-582-0964",
     "Email:": '<a href="mailto:berkererol@gmail.com">berkererol@gmail.com</a>',
   },
@@ -41,7 +41,7 @@ module.exports = {
     {
       title: "Associate Software Engineer",
       company: "OneSpan",
-      location: "Montreal, Canada",
+      location: "Montréal, Canada",
       website: "https://www.onespan.com",
       time: "December 2019 - present",
       badges: [
@@ -50,25 +50,32 @@ module.exports = {
         "TypeScript",
         "Javascript",
         "Storybook",
+        "Material UI",
+        "Jenkins",
+        "BASH",
         "Karma",
         "Node JS",
         "Docker",
         "OpenShift",
         "Kubernetes",
+        "CSS",
+        "JSS",
+        "SASS"
       ],
       contents:
-        "Worked as a frontend developer in the Secure Agreement Automation team that develops a SaaS application for digital authentication, e-signature and audit trail generation for contracts.",
+        "Developer in the frontend team of the Secure Agreement Automation product, a SaaS application for digital authentication, e-signature and audit trail generation for contracts, used by most credit companies in EU.",
       points: [
         "Developed ADA accessibility-compliant components in TypeScript using Angular framework.",
         "Implemented unit tests using Karma and Jasmine.",
         "Integrated frontend application to Gitlab CI/CD pipelines and performed regular deployments.",
         "Collaborated with the back end engineers to connect our frontend to Java API.",
+        "Integrated provider SDKs for face scanning and identity document processing."
       ],
     },
     {
       title: "User Experience Designer",
       company: "Chef Cook It",
-      location: "Montreal, Canada",
+      location: "Montréal, Canada",
       website: "https://www.chefcookit.com",
       time: "May 2019 - August 2019",
       badges: [
@@ -78,15 +85,15 @@ module.exports = {
         "Invision",
         "Behance",
         "Canva",
-        "Affinity Designer",
         "Photoshop",
         "Adobe Premiere Pro",
         "Abstract",
       ],
       contents:
-        "Worked as a UX/UI designer for this emerging Montreal-based meal-kit company and developed their website's interface.",
+        "Reconceived the user experience with a meticulous design system, which boosted client engagement for this emerging Montréal start-up.",
       points: [
-        "Designed mobile-first user interfaces on Figma and Sketch with Abstract.",
+        "Created a new menu page layout after an in-depth A/B testing, which increased new subscriptions and client retention.",
+        "Designed mobile-first interface components on Figma for the front end application in React JS.",
         "Lead the manufacturing process for Kit durable, a zero-waste food kit delivered to customers by bike.",
         "Collaborated with the marketing team to develop new branding guidelines and social media templates.",
       ],
