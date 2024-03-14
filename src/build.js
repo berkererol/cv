47a1c6f263d623a128cc740872d0b5832239f4d6
berkererol <berkererol@gmail.com>
2024-03-14 12:01:03 -0400

    pdf fixes.

--- src/build.js
+++ src/build.js
@@ -22,8 +22,8 @@ handlebars.registerHelper('markdown', markdownHelper)
 const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8')
 const template = handlebars.compile(source)
 const pdfFileName = `${getSlug(templateData.name)}_${getSlug(
-  templateData.title
-)}.pdf`
+  templateData.title1
+)}_${getSlug(templateData.title2)}.pdf`
 const html = template({
   ...templateData,
   baseUrl: `https://${username()}.github.io/${repoName.sync()}`,
